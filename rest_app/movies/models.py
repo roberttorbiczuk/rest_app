279fc1f9e1a4d2bcc89412a389de5ce4c95ada88
Robert Torbiczuk <trobert93@gmail.com>
2018-03-25 14:07:57 +0200

    add Person, Movie, Role models

--- rest_app/movies/models.py
+++ rest_app/movies/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    director = models.ForeignKey(Person)
+    actors = models.ManyToManyField(Person, through='Role', related_name='movie_actors')
+    year = models.IntegerField()
+
+
+class Role(models.Model):
+    role_name = models.CharField(max_length=128)
+    person = models.ForeignKey(Person)
+    movie = models.ForeignKey(Movie)
+
